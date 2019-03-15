72b498088ec798b677309095655ac640cbea2a8a
Christen Ford <christen0ford@gmail.com>
2019-03-14 20:26:46 -0400

    update q1

---
+++ Lab3/q1.py
@@ -0,0 +1,45 @@
+#! /usr/bin/python3
+
+# plots the throughput versus link delay 
+import matplotlib.pyplot as plt
+
+# setup the delay windows for the entire simulation
+#   the components for each window are: (begin_time, end_time, delay)
+windows = [(0, 1.75, 20), (1.75, 3.5, 2), (3.5, 5.25, 4), (5.25, 7.0, 6), 
+        (7.0, 8.75, 8), (8.75, 10.5, 10), (10.5, 12.25, 12), 
+        (12.25, 14.0, 14), (14.0, 15.75, 16), (15.75, 17.5, 18), 
+        (17.5, 20.0, 20)]
+
+# store the total bits received in the windows
+bytes_recvd = dict()
+for window in windows:
+    bytes_recvd[(window[0], window[1])] = 0
+
+# read in the data for the simulation
+with open('q1_a1_p.tr', 'r') as f:
+    for line in f:
+        parts = line.split(' ')
+        # we only care about ones that were received
+        if parts[0] != 'r':
+            continue
+        time = float(parts[1])
+        index = 0
+        for window in windows:
+            if time >= window[0] and time < window[1]:
+                break
+            index = index + 1
+        key = (windows[index][0], windows[index][1])
+        if key in bytes_recvd:
+            bytes_recvd[key] = bytes_recvd[key] + int(parts[5])
+        else:
+            bytes_recvd[key] = int(parts[5])
+
+y = [(v*8)/1000000 for k, v in bytes_recvd.items()]
+x = [window[2] for window in windows]
+
+plt.plot(x, y)
+plt.title('Throughput versus Link Delay')
+plt.xlabel('Link Delay (ms)')
+plt.ylabel('Throughput (Mb/1.75s')
+plt.show()
+
--- Lab3/q1.tcl
+++ Lab3/q1.tcl
@@ -7,10 +7,11 @@ $ns namtrace-all $nf
 
 # define a 'finish' procedure
 proc finish {} {
-    global ns nf
+    global ns nf tr_a1_p
     $ns flush-trace
     close $nf
-    exec nam -a q1.nam &
+    close $tr_a1_p
+    exec python3 q1.py
     exit 0
 }
 
@@ -34,6 +35,10 @@ set reply_link [$ns link $q $b1]
 $ns queue-limit $a1 $p 10
 $ns queue-limit $p $q 10
 $ns queue-limit $q $b1 10
+
+# setup throughput tracing on the last hop link
+set tr_a1_p [open "q1_a1_p.tr" w]
+$ns trace-queue $a1 $p $tr_a1_p
 
 # monitor the queues for the links
 $ns duplex-link-op $p $q queuePos 0.5
@@ -65,11 +70,15 @@ $exp1 attach-agent $tcp1
 $ns at 1/10 "$exp1 start"
 
 # schedule propagation delay updates
+# link delay increase occur in steps of 2 ms from 2ms to 20ms
+# each increase occurs at: 
+# 1.75 3.5 5.25 7.0 8.75 10.5 12.25 14.0 15.75 17.5 seconds 
+#   during the simulation
 for {set i 1} {$i < 11} {incr i} {
     # send_link, carrier_link, reply_link
-    $ns at $i*2 "$send_link set delay_ [expr $i*2]ms"
-    $ns at $i*2 "$carrier_link set delay_ [expr $i*2]ms"
-    $ns at $i*2 "$reply_link set delay_ [expr $i*2]ms"
+    $ns at [expr $i * 1.5 + $i * 0.25] "$send_link set delay_ [expr $i * 2]ms"
+    $ns at [expr $i * 1.5 + $i * 0.25] "$carrier_link set delay_ [expr $i * 2]ms"
+    $ns at  [expr $i * 1.5 + $i * 0.25] "$reply_link set delay_ [expr $i * 2]ms"
 }
 
 # call finish after 20 seconds of sim time
